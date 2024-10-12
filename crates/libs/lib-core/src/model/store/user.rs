bdee19cfc946e266a0845a47be26aa846bf62d86
oleg.ostanin <oleg.ostanin@edna.ru>
2024-10-12 15:55:46 +0300

    now insert works

--- crates/libs/lib-core/src/model/store/user.rs
+++ crates/libs/lib-core/src/model/store/user.rs
@@ -8,14 +8,20 @@ pub struct UserBmc;
 //todo return identity type
 const INSERT_USER: &str = r#"
 INSERT INTO users
-(identity, first_name, last_name, pwd, pwd_salt, token_salt
+(identity, first_name, last_name, pwd
+  --, pwd_salt, token_salt
   --, created_at, updated_at todo
 )
 VALUES
-($1, $2, $3, $4, $5, $6
+($1, $2, $3, $4
+  -- , $5, $6
   -- , $7, $8 todo
 )
 RETURNING id;
+"#;
+
+const SELECT_BY_ID: &str = r#"
+SELECT * FROM users WHERE id=$1;
 "#;
 
 impl UserBmc {
@@ -25,8 +31,28 @@ impl UserBmc {
         user: UserForCreate,
     ) -> Result<u64> {
         //let res = db_client.execute(&statement, &[&user.uuid, &user.pass]).await?;
+        let res = mm.client().execute(INSERT_USER, &[&user.identity, &user.first_name,
+            &user.last_name, &user.password, ]).await;
+
+        match res {
+            Ok(val) => {
+                println!("{:?}", val);
+            }
+            Err(e) => {
+                println!("{:?}", e);
+            }
+        }
+
+        Ok(0)
+    }
+
+    pub async fn get_by_id(
+        mm: &ModelManager,
+        user: UserForCreate,
+    ) -> Result<u64> {
+        //let res = db_client.execute(&statement, &[&user.uuid, &user.pass]).await?;
         let res = Ok(mm.client().execute(INSERT_USER, &[&user.identity, &user.first_name, &user.last_name, &user.password,
-        &"salt", &"token_salt"])
+            &"salt", &"token_salt"])
             .await?);
 
         println!("{:?}", res);
