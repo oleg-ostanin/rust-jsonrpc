3f98956aec7351f54b991bb8360942ec333728cb
oleg.ostanin <oleg.ostanin@edna.ru>
2024-12-08 18:53:44 +0300

    first macro

---
+++ crates/tests/edu/src/macros/hw.rs
@@ -0,0 +1,33 @@
+#[macro_export]
+macro_rules! all_true {
+    ( $( $x:expr ),* ) => {
+        {
+            let mut res = true;
+            $(
+                if(!$x) {
+                    res = false;
+                }
+            )*
+            res
+        }
+    };
+}
+
+fn always_true() -> bool {
+    true
+}
+
+fn equals(x: i32, y: i32) -> bool {
+    x == y
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::macros::hw::{always_true, equals};
+
+    #[test]
+    fn check_macro() {
+        let res = all_true!(always_true(), equals(2, 2), equals(3, 3));
+        println!("res: {:?}", res);
+    }
+}
