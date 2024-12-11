260620298d23960c224ea2bbedc396add0040d46
oleg.ostanin <oleg.ostanin@edna.ru>
2024-12-11 08:19:01 +0300

    starting to experiment

--- crates/tests/procedural-macro/src/lib.rs
+++ crates/tests/procedural-macro/src/lib.rs
@@ -22,3 +22,25 @@ fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
     };
     gen.into()
 }
+
+#[proc_macro_derive(HelloBuilder)]
+pub fn hello_builder_derive(input: TokenStream) -> TokenStream {
+    // Construct a representation of Rust code as a syntax tree
+    // that we can manipulate
+    let ast = syn::parse(input).unwrap();
+
+    // Build the trait implementation
+    crate::impl_hello_builder(&ast)
+}
+
+fn impl_hello_builder(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl HelloMacro for #name {
+            fn hello_macro() {
+                println!("Hello, Macro! My name is {}!", stringify!(#name));
+            }
+        }
+    };
+    gen.into()
+}
